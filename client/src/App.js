efd950edde58a1600bdf616256580385dd0a924b
carlinjshaw <carlinjshaw@gmail.com>
2021-10-04 20:13:30 -0400

    client side graphQL replacements

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,25 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
+  uri: '/graphql'
+});
 
 function App() {
   return (
-    <Router>
+    <ApolloProvider clinet={clinet}>    <Router>
       <>
         <Navbar />
         <Switch>
@@ -16,6 +31,7 @@ function App() {
         </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
